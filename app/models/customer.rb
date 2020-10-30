8958bab216cc5969c49db7cd95fa998cf68e5785
s-itonaga <sitonaga914@gmail.com>
2020-10-30 12:14:39 +0900

    メニュー画面を修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+
+
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "customers#index"
   resources :users, only: [:edit, :update]
+  resources :customers
 end
