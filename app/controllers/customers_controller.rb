fc35844f0595a26bbc26cf3234bbc3ebe487032d
s-itonaga <sitonaga914@gmail.com>
2020-11-03 18:33:34 +0900

    お客様編集画面実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -20,6 +20,16 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def customer_params
