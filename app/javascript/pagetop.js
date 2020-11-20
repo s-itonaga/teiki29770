7e5af8d0306c69363ab6df7f01964ebec19e3da6
s-itonaga <sitonaga914@gmail.com>
2020-11-21 08:18:39 +0900

    スクロール途中にボタンを出現させる記述を追加

--- app/javascript/pagetop.js
+++ app/javascript/pagetop.js
@@ -1,5 +1,17 @@
 $(function(){
+  //ページトップへのスクロール
   $('#pagetop').on('click',(function () {
-    $("html,body").animate({scrollTop:0},"300");
+    //id名#pagetopがクリックされたら、以下の処理を実行
+    $("html,body").animate({scrollTop:0},"200");
   }));
+  $('#pagetop').hide();
+  //ページトップの出現
+  $(window).on('scroll',(function () {
+      if($(window).scrollTop() > 0) {
+          $('#pagetop').slideDown(200);
+      } else {
+          $('#pagetop').slideUp(200);
+      }
+  }));
+
 });
