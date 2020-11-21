e532ff3bf660d771b1bb5f82da7bbcc8d4a79400
s-itonaga <sitonaga914@gmail.com>
2020-11-21 20:14:26 +0900

    jQueryの記述を変更

--- app/javascript/pagetop.js
+++ app/javascript/pagetop.js
@@ -7,16 +7,17 @@ $(function(){
   $('#pagetop').hide();
   //ページトップの出現
   $(window).on('scroll',(function () {
-      if($(window).scrollTop() > 0) {
-          $('#pagetop').slideDown(200);
+      if($(window).scrollTop() > 10) {
+          $('#pagetop').slideDown(600);
       } else {
           $('#pagetop').slideUp(200);
       }
   }));
-  //
+  //マウスオーバー時の色変化
   $('#pagetop').on("mouseover", function () {
     $(this).css({ "background-color": "gray" });
   }).on("mouseout", function () {
     $(this).css({ "background-color": "#ccc" });
   });
 });
+
