1b3dd2341d32b38410c719372084249e345a50ec
s-itonaga <sitonaga914@gmail.com>
2020-11-23 20:07:24 +0900

    曜日で色を変更

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,2 +1,11 @@
 module CalendarsHelper
+  def wday_color(calendar)
+    if calendar.day.wday == 0
+      return 'red'
+    elsif calendar.day.wday == 6
+      return 'blue'
+    else
+      return 'gray'
+    end
+  end  
 end
