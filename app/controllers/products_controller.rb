044fc2af18a327b938fd92d314bf29f2e31d3791
s-itonaga <sitonaga914@gmail.com>
2020-11-26 20:30:53 +0900

    取置商品登録画面を実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,21 @@ class ProductsController < ApplicationController
 
   def new
     @customer = Customer.find(params[:customer_id])
-    @product 
+    @product = Product.new
   end
 
   def create
+    @customer = Customer.find(params[:customer_id])
+    @product = @customer.products.new(product_params)
+    if @product.save
+      redirect_to edit_customer_path(@customer), notice: "#{@customer.name}様の取置商品を登録しました。"
+    else
+      render :new
+    end
+  end
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :z_code, :num, :release).merge(customer_id: @customer.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :customer
+
+  with_options presence: true do
+    validates :z_code, numericality: { only_integer: true }, length: { maximum: 5 }
+    validates :num, numericality: { only_integer: true }, length: { maximum: 5 }
+    validates :release
+  end
+
 end
