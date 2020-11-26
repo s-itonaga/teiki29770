41ac8973396b2a292972693afcd96dcf1320618a
s-itonaga <sitonaga914@gmail.com>
2020-11-26 19:32:40 +0900

    取置き商品一覧と登録画面を仮作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+  def index
+    @customer = Customer.find(params[:customer_id])
+    @produsts = @customer.products
+  end
+
+  def new
+    @customer = Customer.find(params[:customer_id])
+    @product 
+  end
+
+  def create
+  end
 end
