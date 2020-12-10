bc1c9513f3ab72fb8b497a64e3e4ccc1bf85954f
s-itonaga <sitonaga914@gmail.com>
2020-12-10 19:24:49 +0900

    入荷物商品一覧画面のソート機能を実装

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,9 @@
 class StocksController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
     @calendar = Calendar.find(params[:calendar_id])
-    @stocks = @calendar.stocks
+    @stocks = @calendar.stocks.order("#{sort_column} #{sort_direction}")
     @products = Product.all
   end
 
@@ -14,4 +16,15 @@ class StocksController < ApplicationController
     Stock.csv_import(params[:file], params[:calendar_id])
     redirect_to root_path, notice: "#{l @calendar.day, format: :long}の入荷物を登録しました。"
   end
+
+  private
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+  end
+
+  def sort_column
+    Stock.column_names.include?(params[:sort]) ? params[:sort] : 'id'
+  end
+
 end
