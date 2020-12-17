98c5abc17eca850a24142f11dbf98224bccfa3e6
s-itonaga <sitonaga914@gmail.com>
2020-12-17 20:42:25 +0900

    記述ミスを修正

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,6 +1,6 @@
 module StocksHelper
   def sort_order(column, title)
-    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    css_class = (column == sort_column) ? "sort_current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == 'asc') ? 'desc' : 'asc'
     link_to title, { sort: column, direction: direction }, class: "sort_header #{css_class}"
   end
